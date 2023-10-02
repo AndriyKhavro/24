02b0cc68471ff6f7587f7e2ffa8f45c1d2d130ff
Andriy Khavro <akh@imtc.com>
2023-10-02 12:32:55 +0300

    Initial commit

---
+++ HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ec2Test;
+
+[Route("api/health")]
+[ApiController]
+public class HealthController
+{
+    [HttpGet]
+    public string Health()
+    {
+        return Environment.MachineName;
+    }
+}
